330a7490e2345b5eb9fdb4fc2f9ccb8c1c7b088a
Finn Boire <finnegan.mccool@gmail.com>
2017-05-12 16:48:35 -0700

    Rewrite link parser, change to csv
    * Link parser now gets rid of tables
    * Locates paragraph better
    * Increase values logged by the wiki-connections file to facilitate graphing and debugging

--- parser.py
+++ parser.py
@@ -1,41 +1,25 @@
 import urllib.request
 import re as regex
-
-def LinkParse(paragraph):
-  pointy_bracket = 0
-  circle_bracket = 0
-  happy_paragraph = ''
-  for i in paragraph:
-    if i is "<":
-      pointy_bracket += 1
-    elif i is ">":
-      pointy_bracket -= 1
-    elif i is "(":
-      circle_bracket += 1
-    elif i is ")":
-      circle_bracket -= 1
-    print(str(circle_bracket) + " : " + str(pointy_bracket) + " : " + i)
-  return happy_paragraph
+import csv
 
 def GetPageData(url):
-  #response = urllib.request.urlopen("https://en.wikipedia.org" + url)
-  #html = response.read().decode('utf-8')
+  response = urllib.request.urlopen("https://en.wikipedia.org" + url)
+  html = response.read().decode('utf-8')
 
-  response = open("Nepal")
-  html = response.read()
-  debug = open("debug_txt", 'a')
+  #response = open("local_html/Nepal")
+  #html = response.read()
+  #debug = open("debug_txt", 'a')
  
   name_content = ''.join(regex.findall('<h1(.*?)<\/h1>', html))
-  name = regex.findall('>(.*)', name_content)
+  name_content = ''.join(regex.findall('>(.*)', name_content))
+  name = name_content.replace('<i>', '').replace('<\/i>', '')
  
-#  link_content = html.split('<div id="mw-content-text"', 1)[1]
+  link_content = html.split('<div id="mw-content-text"', 1)[1]
   link_content = regex.sub('<table[\s\S]+?<\/table>', '', html)
   link_content = link_content.split('<p>', 1)[1]
   link_content = link_content.split('<\/p>', 1)[0]
   link_content = regex.sub('<sup(.*?)<\/sup>', '', link_content)
-  link_content = LinkParse(link_content)
-  debug.write(link_content)
-  #link_content = regex.sub('\([^\)]*(?!\))\<a(.+?)a\>(.*?)\)', '', link_content)
+  link_content = regex.sub('\([^\)]*(?!\))\<a(.+?)a\>(.*?)\)', '', link_content)
   has_link = "<a href=" in link_content
   link_content = link_content.split('<a', 1)[1]
   link_content = link_content.split('</a>', 1)[0]
@@ -43,37 +27,43 @@ def GetPageData(url):
   link_is_sane = bool(regex.search('^\/wiki\/(.*)$', link))
   has_link = has_link and link_is_sane
  
-  page_data = []
+  page_data = {}
  
-  #page_data.append(response.geturl())
-  page_data.append('')
-  page_data.append(''.join(name))
-  page_data.append(link)
-  page_data.append(has_link)
+  page_data['page_link'] = response.geturl()
+  page_data['page_name'] = ''.join(name)
+  page_data['next_page_link'] = link
+  page_data['has_link'] = has_link
 
   return page_data
 
 database_file_writer = open('wiki_connections', 'a')
+writer = csv.DictWriter(database_file_writer, fieldnames=['page_link', 'page_name', 'next_page_link', 'has_link'])
 
 def IsInDB(url):
   with open("wiki_connections", 'r') as database_file_reader:
-    for line in database_file_reader:
-      if url in line:
+    data = csv.reader(database_file_reader)
+    for row in data:
+      if row[0] == "https://en.wikipedia.org" + url:
         return True
     return False
 
 def AddToDB(page_data):
-  database_file_writer.write(page_data[0] + ' : ' + page_data[1] + ' : ' + page_data[2] + ' : ' + str(page_data[3]) + '\n')
-  database_file_writer.flush()
+  writer.writerow(page_data)
+  if not page_data['has_link']:
+    database_file_writer.write("ERROR: Failed to parse link\n")
 
 def Recurse(page_data):
-  if not IsInDB(page_data[2]):
+  if not IsInDB(page_data['next_page_link']):
     AddToDB(page_data)
-    new_page_data = GetPageData(page_data[2])
+    new_page_data = GetPageData(page_data['next_page_link'])
     print(new_page_data)
-    Recurse(new_page_data)
+    if new_page_data['has_link']:
+      Recurse(new_page_data)
+  else:
+    AddToDB(page_data)
 
-#while True:
-initial_url = '/wiki/Special:Random'
-#Recurse(GetPageData(initial_url))
-GetPageData('')
+
+while True:
+  print("Starting new one")
+  initial_url = '/wiki/Special:Random'
+  Recurse(GetPageData(initial_url))
